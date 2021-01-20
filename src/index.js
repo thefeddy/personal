05ebe5917e114fa81957dab4ecc72a86b5933522
David Friedrich <thefeddy@gmail.com>
2021-01-19 22:23:27 -0500

    some pre-clean up work with nodejks

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
-const port = 3000;
-const express = require('express');
 const hbs = require('express-hbs');
-
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
@@ -14,6 +13,4 @@ app.get('/', (req, res) => {
     res.send('Hello s!');
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
+app.listen(process.env.PORT, () => { });
