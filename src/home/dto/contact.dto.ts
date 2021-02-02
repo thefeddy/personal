c232c0fb36748805d8402e6ed27cdb4ac25644a9
David Friedrich <thefeddy@gmail.com>
2021-02-02 01:36:50 -0500

    back-end work for contact form

---
+++ src/home/dto/contact.dto.ts
@@ -0,0 +1,6 @@
+export class ContactDTO {
+    full_name: string;
+    email: string;
+    phone: string;
+    message: string;
+}
--- src/home/home.controller.ts
+++ src/home/home.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, Render, Res } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Post, Render, Res } from '@nestjs/common';
 import { MailerService } from '@nestjs-modules/mailer';
 
 import { Response } from 'express';
 
+
+import { ContactDTO } from './dto/contact.dto';
 
 @Controller('')
 export class HomeController {
@@ -11,14 +13,31 @@ export class HomeController {
     @Get('/')
     @Render('home/index')
     async index(@Res() res: Response): Promise<any> {
-        // await this.mailerService.sendMail({
-        //     to: 'thefeddy@gmail.com',
-        //     from: 'no-reply@dfriedrich.dev',
-        //     subject: 'Resume Contact Form',
-        //     template: 'src/views/emails/resume',
-        //     context: {
 
-        //     },
-        // });
+    }
+
+
+    @Post('/mail/')
+    async sendMail(
+        @Body() contactDTO: ContactDTO,
+        @Res() res: Response
+    ): Promise<any> {
+        console.log(contactDTO);
+        await this.mailerService.sendMail({
+            to: 'thefeddy@gmail.com',
+            from: 'no-reply@dfriedrich.dev',
+            subject: `RCF : ${contactDTO.full_name} `,
+            template: 'src/views/emails/resume',
+            context: {
+                name: contactDTO.full_name,
+                email: contactDTO.email,
+                phone: contactDTO.phone,
+                message: contactDTO.message
+            },
+        });
+
+        return res.status(HttpStatus.OK).json({
+            status: HttpStatus.OK
+        });
     }
 }
